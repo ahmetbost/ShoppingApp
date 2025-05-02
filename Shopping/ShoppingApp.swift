5047a4673a169b02da7b2d9d410d90c1ae07fcf9
Bost <ahmetbostanciogluu@gmail.com>
2025-05-02 15:14:13 +0300

    Initial Commit

---
+++ Shopping/ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingApp.swift
+//  Shopping
+//
+//  Created by Ahmet Bostancıoğlu on 2.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
